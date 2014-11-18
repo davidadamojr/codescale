dc90243fd3fe773b47921cd3e7bd779a744c8b25
David Adamo <me@davidadamojr.com>
2014-11-18 08:39:06 -0600

    additional views

--- app/js/refresher/refresher.js
+++ app/js/refresher/refresher.js
@@ -1,14 +1,23 @@
 'use strict';
 
-angular.module('codeScaleApp.refresher', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/refresher', {
-    templateUrl: 'partials/refresher/refresher.html',
-    controller: 'RefresherCtrl'
-  });
-}])
-
-.controller('RefresherCtrl', [function() {
-
+angular.module('codeScaleApp.refresher', ['ngRoute']).config(['$routeProvider',
+function($routeProvider) {
+	$routeProvider.when('/refresher', {
+		templateUrl : 'partials/refresher/refresher.html',
+		controller : 'RefresherCtrl'
+	});
+}]).controller('RefresherCtrl', ['$scope',
+function($scope) {
+	
+	$scope.currentSnippet = 0;
+	$scope.snippets = [{
+		snippetText : "This is snippet 1",
+		output : "65"
+	}, {
+		snippetText : "This is snippet 2",
+		output : "82"
+	}, {
+		snippetText : "This is snippet 3",
+		output : "71"
+	}];
 }]);
