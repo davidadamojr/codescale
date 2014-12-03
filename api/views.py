7d67f3a057099d8a22e46304b65e0be02092c733
David Adamo Jr <me@davidadamojr.com>
2014-12-02 20:32:26 -0600

    api endpoints implemented

--- api/views.py
+++ api/views.py
@@ -7,27 +7,59 @@ from models import AccessCode, Snippets, Trials
 parser = reqparse.RequestParser()
 parser.add_argument('code', type=str, help='Code must be a string')
 parser.add_argument('time', type=int, help='Time (in seconds) must be a number')
-parser.add_argument('id', type=int, help='id must be a number')
+parser.add_argument('snippet_id', type=int, help='id must be a number')
 parser.add_argument('output', type=string)
 parser.add_argument('is_correct', type=int)
 
 class AccessView(restful.Resource):
   def post(self):
 	args = parser.parse_args()
-    pass
+	access_code = AccessCode.query.filter_by(code=args['code'])
+	access_code.used = 1
+	db.session.commit()
+	response = {'error': False, 'authorized': True},
+	return response
 
 class RefreshersView(restful.Resource):
   def get(self):
-    pass
+    response = {}
+	refreshers = Snippets.query.filter_by(type='REFRESHER').all()
+	response['error'] = False
+	response['snippets'] = []
+	for snippet in response:
+	  file_handle = open(snippet.filename, 'r')
+	  file_contents = file_handle.read()
+      snippet_details = {'snippet':file_contents, 'output':snippet.output}
+	  response['snippets'].append(snippet_details)
+	  file_handle.close()
+	  
+	return response
   
 class ActivityView(restful.Resource):
   def get(self):
-    pass
+    response = {}
+	activities = Snippets.query.filter(Snippets.type != 'REFRESHER').all()
+	response['error'] = False
+	response['snippets'] = []
+	for snippet in activities:
+	  file_handle = open(snippet.filename, 'r')
+	  file_contents = file_handle.read()
+	  snippet_details = {'snippet':file_contents, 'output':snippet.output}
+	  response['snippets'].append(snippet_details)
+	  file_handle.close()
+	
+	return response
 
 class SnippetView(restful.Resource):
   def post(self):
 	args = parser.parse_args()
-    pass
+	response = {}
+	trial = Trials(args['code'], args['snippet_id'], args['time'], args['is_correct'])
+	new_trial = db.session.add(trial)
+	response['error'] = False
+	response['attempt'] = {'code':new_trial.access_code, 'snippet_id':new_trial.snippet_id, 'time':new_trial.time_elapsed, 'is_correct':new_trial.is_correct}
+    
+	return response
 
 api.add_resource(AccessView, '/api/v1/access/')
 api.add_resource(RefreshersView, '/api/v1/refreshers')
