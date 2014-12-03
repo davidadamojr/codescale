d5a58230f87c3da341809075948f57ec34095625
David Adamo Jr <me@davidadamojr.com>
2014-12-03 17:27:18 -0600

    database update

--- codescale.sql
+++ codescale.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2014 at 05:40 AM
+-- Generation Time: Dec 04, 2014 at 12:18 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -32,7 +32,8 @@ CREATE TABLE IF NOT EXISTS `access_codes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(12) NOT NULL,
   `used` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -45,8 +46,29 @@ CREATE TABLE IF NOT EXISTS `snippets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(45) NOT NULL,
   `filename` varchar(45) NOT NULL,
+  `output` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `snippets`
+--
+
+INSERT INTO `snippets` (`id`, `type`, `filename`, `output`) VALUES
+(1, 'SWITCH_CASE', 'case_activity_1_7lines.txt', '10'),
+(2, 'DO_WHILE', 'do_activity_1_8lines.txt', '10'),
+(3, 'REFRESHER', 'do_refresher.txt', '10'),
+(4, 'FOR_LOOP', 'for_activity_1_5lines.txt', '10'),
+(5, 'REFRESHER', 'for_refresher.txt', '10'),
+(6, 'IF_ELSE', 'if_activity_1_4lines.txt', '10'),
+(7, 'REFRESHER', 'if_refresher.txt', '10'),
+(8, 'RECURSION', 'recursion_activity_1_10lines.txt', '10'),
+(9, 'REFRESHER', 'recursion_refresher.txt', '10'),
+(10, 'SEQ_13', 'seq_activity_13lines.txt', '10'),
+(11, 'REFRESHER', 'seq_refresher.txt', '10'),
+(12, 'REFRESHER', 'switch_refresher.txt', '10'),
+(13, 'WHILE_LOOP', 'while_activity_1_8lines.txt', '10'),
+(14, 'REFRESHER', 'while_refresher.txt', '10');
 
 -- --------------------------------------------------------
 
@@ -56,13 +78,11 @@ CREATE TABLE IF NOT EXISTS `snippets` (
 
 CREATE TABLE IF NOT EXISTS `trials` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `access_code_id` int(11) NOT NULL,
+  `access_code` varchar(12) NOT NULL,
   `snippet_id` int(11) NOT NULL,
   `time_elapsed` int(11) NOT NULL,
   `is_correct` tinyint(4) NOT NULL,
-  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `access_code_id` (`access_code_id`,`snippet_id`),
   KEY `snippet_id` (`snippet_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -74,8 +94,7 @@ CREATE TABLE IF NOT EXISTS `trials` (
 -- Constraints for table `trials`
 --
 ALTER TABLE `trials`
-  ADD CONSTRAINT `trials_ibfk_2` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `trials_ibfk_1` FOREIGN KEY (`access_code_id`) REFERENCES `access_codes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `trials_ibfk_2` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
