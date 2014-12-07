95e45f65226e16cb59d0efcd2cd7b207f6f97fa1
David Adamo <me@davidadamojr.com>
2014-12-07 11:11:21 -0600

    database updates

--- codescale.sql
+++ codescale.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2014 at 08:47 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Generation Time: Dec 07, 2014 at 05:10 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `access_codes` (
 
 INSERT INTO `access_codes` (`id`, `code`, `used`) VALUES
 (1, 'abcd1234', 1),
-(2, 'dcta1234', 0);
+(2, 'dcta1234', 1);
 
 -- --------------------------------------------------------
 
@@ -90,16 +90,19 @@ CREATE TABLE IF NOT EXISTS `survey` (
   `proficiency` int(11) NOT NULL,
   `experience` varchar(10) NOT NULL,
   `education` varchar(45) NOT NULL,
+  `interrupted` tinyint(4) NOT NULL,
+  `when` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `survey`
 --
 
-INSERT INTO `survey` (`id`, `code`, `proficiency`, `experience`, `education`) VALUES
-(1, 'abcd1234', 3, '>4', 'PhD Student'),
-(2, 'abcd1234', 3, '>4', 'PhD Student');
+INSERT INTO `survey` (`id`, `code`, `proficiency`, `experience`, `education`, `interrupted`, `when`) VALUES
+(1, 'abcd1234', 3, '>4', 'PhD Student', 0, '2014-12-07 09:03:18'),
+(2, 'abcd1234', 3, '>4', 'PhD Student', 0, '2014-12-07 09:03:18'),
+(3, 'dcta1234', 4, '<1', 'High School', 1, '2014-12-07 10:10:31');
 
 -- --------------------------------------------------------
 
@@ -115,14 +118,66 @@ CREATE TABLE IF NOT EXISTS `trials` (
   `is_correct` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `snippet_id` (`snippet_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
 
 --
 -- Dumping data for table `trials`
 --
 
 INSERT INTO `trials` (`id`, `access_code`, `snippet_id`, `time_elapsed`, `is_correct`) VALUES
-(1, 'abcd1234', 3, 23, 3);
+(2, 'abcd1234', 1, 28, 0),
+(3, 'abcd1234', 1, 28, 1),
+(4, 'abcd1234', 2, 28, 1),
+(5, 'abcd1234', 4, 28, 1),
+(6, 'abcd1234', 6, 28, 1),
+(7, 'abcd1234', 8, 28, 1),
+(8, 'abcd1234', 10, 28, 1),
+(9, 'abcd1234', 1, 28, 1),
+(10, 'abcd1234', 2, 28, 1),
+(11, 'abcd1234', 4, 28, 1),
+(12, 'abcd1234', 6, 28, 1),
+(13, 'abcd1234', 8, 28, 1),
+(14, 'abcd1234', 10, 28, 1),
+(15, 'dcta1234', 1, 28, 1),
+(16, 'dcta1234', 2, 28, 1),
+(17, 'dcta1234', 4, 28, 1),
+(18, 'dcta1234', 6, 28, 1),
+(19, 'dcta1234', 8, 28, 1),
+(20, 'dcta1234', 10, 28, 1),
+(21, 'abcd1234', 1, 28, 1),
+(22, 'abcd1234', 2, 28, 1),
+(23, 'abcd1234', 4, 28, 1),
+(24, 'abcd1234', 6, 28, 1),
+(25, 'abcd1234', 8, 28, 1),
+(26, 'abcd1234', 10, 28, 1),
+(27, 'abcd1234', 1, 28, 1),
+(28, 'abcd1234', 2, 28, 1),
+(29, 'abcd1234', 4, 28, 1),
+(30, 'abcd1234', 6, 28, 1),
+(31, 'abcd1234', 8, 28, 1),
+(32, 'abcd1234', 10, 28, 1),
+(33, 'dcta1234', 1, 28, 1),
+(34, 'dcta1234', 2, 28, 1),
+(35, 'dcta1234', 4, 28, 1),
+(36, 'dcta1234', 6, 28, 1),
+(37, 'dcta1234', 8, 28, 1),
+(38, 'dcta1234', 10, 28, 1),
+(39, 'abcd1234', 1, 28, 1),
+(40, 'abcd1234', 2, 28, 0),
+(41, 'abcd1234', 2, 28, 0),
+(42, 'abcd1234', 2, 28, 0),
+(43, 'abcd1234', 2, 28, 0),
+(44, 'abcd1234', 2, 28, 1),
+(45, 'abcd1234', 4, 28, 1),
+(46, 'abcd1234', 6, 28, 1),
+(47, 'abcd1234', 8, 28, 1),
+(48, 'abcd1234', 10, 28, 1),
+(49, 'dcta1234', 1, 28, 1),
+(50, 'dcta1234', 2, 28, 1),
+(51, 'dcta1234', 4, 28, 1),
+(52, 'dcta1234', 6, 28, 1),
+(53, 'dcta1234', 8, 28, 1),
+(54, 'dcta1234', 10, 28, 1);
 
 --
 -- Constraints for dumped tables
